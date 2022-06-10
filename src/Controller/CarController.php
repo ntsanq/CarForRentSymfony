f54d0d69b7b09a90e268a9f35110b56282f1f7e0
ntsanq <ntsanq@gmail.com>
2022-06-10 17:52:48 +0700

    feat: create add car,find car function

---
+++ src/Controller/CarController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Car;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarController extends AbstractController
+{
+    #[Route('/car', name: 'app_car')]
+    public function index(): Response
+    {
+        return $this->render('car/index.html.twig', [
+            'controller_name' =>    'CarController',
+        ]);
+    }
+
+    /**
+     * @Route("/addCar", name="add_car")
+     */
+    public function addCar(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new Car();
+        $product->setName('Vis');
+        $product->setDescription('This car is good!');
+        $product->setPrice(122);
+
+        $entityManager->persist($product);
+
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
+    /**
+     * @Route("/car/{id}", name="show_car")
+     */
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $car = $doctrine->getRepository(Car::class)->find($id);
+        if (!$car) {
+            throw $this->createNotFoundException(
+                'No car found for id '.$id
+            );
+        }
+        return new Response("The car with the id $id is: ".$car->getName());
+    }
+
+    /**
+     * @Route("/car/entity/{id}", name="entity_show_car")
+     */
+    public function showByEntity(Car $car): Response
+    {
+        return new Response("The car with the id is: ".$car->getName());
+    }
+}
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route (
+     *     "/home",
+     *     name = "app_home"
+     * )
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController'
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/home/key",
+     *     name = "home_showKey"
+     * )
+     */
+    public function showKey(ParameterBagInterface $parameterBag): Response
+    {
+        $s3key = $parameterBag->get('s3Secret');
+        return $this->render('/home/key.html.twig',[
+            's3key' => $s3key
+        ]);
+    }
+}
