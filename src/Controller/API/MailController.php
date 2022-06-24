2b43f4f03168fb1a5bf1d37c2555dcf6ec7bdb83
ntsanq <ntsanq@gmail.com>
2022-06-24 22:42:29 +0700

    update: send mail in json request

--- src/Controller/API/MailController.php
+++ src/Controller/API/MailController.php
@@ -4,19 +4,25 @@ namespace App\Controller\API;
 
 use App\Service\MailService;
 use App\Traits\JsonResponseTrait;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api', name: 'mail_')]
 class MailController
 {
+    const HELLO_FILE = __DIR__ . "/../../../public/file/hello.html";
+
     use JsonResponseTrait;
 
-    #[Route('/mail', name: 'index')]
-    public function index(MailService $mailService)
+    #[Route('/mail', name: 'index', methods: 'POST')]
+    public function index(MailService $mailService, Request $request)
     {
-        $mailService->mail();
+        $bodyRequest = json_decode($request->getContent(), true);
+        $userMail = $bodyRequest['userMail'];
+        $name = $bodyRequest['name'];
+        $mailService->mail($userMail, self::HELLO_FILE, $name);
         return $this->success([
-            'message'=>'Mail sent'
+            'message' => 'Mail sent'
         ]);
     }
 }
--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -5,11 +5,11 @@ namespace App\Service;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-use Symfony\Component\DependencyInjection\ParameterBag\ContainerBag;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 
 class MailService
 {
+    const SUPPORT_MAIL_NAME = 'NTS Support';
     private ContainerBagInterface $containerBag;
 
     public function __construct(ContainerBagInterface $containerBag)
@@ -17,28 +17,25 @@ class MailService
         $this->containerBag = $containerBag;
     }
 
-    public function mail(): void
+    public function mail($userMail, string $path, $userName = 'User'): void
     {
         $mail = new PHPMailer(true);
         try {
             $mail->SMTPDebug = SMTP::DEBUG_OFF;
             $mail->isSMTP();
-            $mail->Host = 'smtp.zoho.com';
+            $mail->Host = $this->containerBag->get('zohoHost');
             $mail->SMTPAuth = true;
             $mail->Username = $this->containerBag->get('zohoMail');
             $mail->Password = $this->containerBag->get('zohoPassword');
-            $mail->SMTPSecure = 'tls';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
-            //Recipients
-            $mail->setFrom($this->containerBag->get('zohoMail'), 'Support');
-            $mail->addAddress('kha.nguyen@nfq.asia', 'Kha Nguyen');
+            $mail->setFrom($this->containerBag->get('zohoMail'), self::SUPPORT_MAIL_NAME);
+            $mail->addAddress($userMail, $userName);
 
-            //Content
             $mail->isHTML(true);
-            $mail->Subject = "Hello Our Awesome User";
-            $mail->Body = 'Hope you still always in contact with <b>Our shop</b>';
-
+            $mail->Subject = "Hello $userName";
+            $mail->Body = file_get_contents($path);
             $mail->send();
         } catch (Exception $e) {
             throw new \Exception("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
